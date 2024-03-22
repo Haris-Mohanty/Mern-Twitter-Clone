7a12abaac86444275255f1bb9fd38a2ac503a28f
Haris Mohanty <harismohanty8658@gmail.com>
2024-03-22 22:54:31 +0530

    client- Profile page code processing...

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import PageNotFound from "./Pages/PageNotFound";
+import Profile from "./Pages/Profile";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
---
+++ client/src/Pages/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <>
+      <div className="w-[50%]">
+        <div>
+          <img
+            src={
+              "https://pbs.twimg.com/profile_images/1676844366659457024/1qiyi4zB_400x400.jpg"
+            }
+            alt="banner"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
