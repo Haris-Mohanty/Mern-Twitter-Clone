2bbcd22e929d1bfafa546203430b4c47df4571f4
Haris Mohanty <harismohanty8658@gmail.com>
2024-03-22 23:04:06 +0530

    client- All routes added.

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import LeftSidebar from "../components/LeftSidebar";
+import RightSidebar from "../components/RightSidebar";
 
 const Profile = () => {
   return (
     <>
-      <div className="w-[50%]">
-        <div>
-          <img
-            src={
-              "https://pbs.twimg.com/profile_images/1676844366659457024/1qiyi4zB_400x400.jpg"
-            }
-            alt="banner"
-          />
+      <div className="flex justify-between w-[80%] mx-auto">
+        <LeftSidebar />
+        <div className="w-[50%]">
+          <div>
+            <img
+              src={
+                "https://pbs.twimg.com/profile_images/1676844366659457024/1qiyi4zB_400x400.jpg"
+              }
+              alt="banner"
+            />
+          </div>
         </div>
+        <RightSidebar />
       </div>
     </>
   );
--- client/src/components/LeftSidebar.jsx
+++ client/src/components/LeftSidebar.jsx
@@ -6,6 +6,7 @@ import { IoNotificationsOutline } from "react-icons/io5";
 import { CiUser } from "react-icons/ci";
 import { IoBookmarksOutline } from "react-icons/io5";
 import { CiLogout } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 const LeftSidebar = () => {
   return (
@@ -20,30 +21,48 @@ const LeftSidebar = () => {
           />
         </div>
         <div className="my-4">
-          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer">
+          <Link
+            to={"/"}
+            className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer"
+          >
             <GoHome size={"24px"} />
             <h1 className="font-semibold text-lg ml-2">Home</h1>
-          </div>
-          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer">
+          </Link>
+          <Link
+            to={"/explore"}
+            className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer"
+          >
             <CiSearch size={"24px"} />
             <h1 className="font-semibold text-lg ml-2">Explore</h1>
-          </div>
-          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer">
+          </Link>
+          <Link
+            to={"/notifications"}
+            className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer"
+          >
             <IoNotificationsOutline size={"24px"} />
             <h1 className="font-semibold text-lg ml-2">Notifications</h1>
-          </div>
-          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer">
+          </Link>
+          <Link
+            to={"/profile"}
+            className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer"
+          >
             <CiUser size={"24px"} />
             <h1 className="font-semibold text-lg ml-2">Profile</h1>
-          </div>
-          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer">
+          </Link>
+          <Link
+            to={"/bookmarks"}
+            className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer"
+          >
             <IoBookmarksOutline size={"24px"} />
             <h1 className="font-semibold text-lg ml-2">Bookmarks</h1>
-          </div>
-          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer">
+          </Link>
+          <Link
+            to={"/logout"}
+            className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 rounded-full hover:cursor-pointer"
+          >
             <CiLogout size={"24px"} />
             <h1 className="font-semibold text-lg ml-2">Logout</h1>
-          </div>
+          </Link>
           <button className="px-4 py-2 border-none text-md bg-[#109BF0] w-[70%] rounded-full text-white font-bold">
             Post
           </button>
