845709b0aa86812b96a8bf03dc54ad19137cd249
Haris Mohanty <harismohanty8658@gmail.com>
2024-03-23 22:49:04 +0530

    client- Profile page code processing...

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import LeftSidebar from "../components/LeftSidebar";
 import RightSidebar from "../components/RightSidebar";
+import { IoMdArrowBack } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   return (
     <>
       <div className="flex justify-between w-[80%] mx-auto">
         <LeftSidebar />
-        <div className="w-[50%]">
+        <div className="w-[50%] border">
           <div>
+            <div className="flex items-center px-4 py-2">
+              <Link to={"/"} className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
+                <IoMdArrowBack size={"24px"} />
+              </Link>
+              <div className="ml-2">
+                <h1 className="font-bold text-lg">Haris Mohanty</h1>
+                <p className="text-gray-500 text-sm">10 posts</p>
+              </div>
+            </div>
             <img
               src={
-                "https://pbs.twimg.com/profile_images/1676844366659457024/1qiyi4zB_400x400.jpg"
+                "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
               }
               alt="banner"
             />
