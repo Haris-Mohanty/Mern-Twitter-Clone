b03f07ade1ef159541b15bf6e1c81fbbdd5d37ae
Haris Mohanty <harismohanty8658@gmail.com>
2024-04-03 21:49:26 +0530

    server: Routes created for bookmark.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,7 +3,9 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  bookmark,
 } from "../controllers/userController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 //Router Obj
 const router = express.Router();
@@ -17,6 +19,9 @@ router.post("/login", loginUser);
 
 //Logout
 router.get("/logout", logoutUser);
+
+//Bookmarks
+router.put("/bookmark/:id", authMiddleware, bookmark);
 
 //Export
 export default router;
