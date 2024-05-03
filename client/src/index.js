bc2c050c2ef614178c5f654e44c8cecb5f61b7f1
Haris Mohanty <harismohanty8658@gmail.com>
2024-05-03 10:02:15 +0530

    client- code persisting successfully.

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,10 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
@@ -15,7 +19,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
