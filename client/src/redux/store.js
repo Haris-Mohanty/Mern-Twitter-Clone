41f1102296aa3fab53e583ed9c9032183dd35e32
Haris Mohanty <harismohanty8658@gmail.com>
2024-05-03 09:57:41 +0530

    client- code persisting processing....

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,6 +2,24 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { userSlice } from "./userSlice";
 import { spinnerSlice } from "./spinnerSlice";
 import { tweetSlice } from "./tweetSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// Persist
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
 
 //Combine Reducer
 const rootReducer = combineReducers({
@@ -10,8 +28,16 @@ const rootReducer = combineReducers({
   tweets: tweetSlice.reducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;
