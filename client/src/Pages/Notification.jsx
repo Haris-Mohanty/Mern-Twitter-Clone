1205aed495dca6fec25eb20f0256192621c1ee91
Haris Mohanty <harismohanty8658@gmail.com>
2024-05-11 11:48:37 +0530

    client- Seen and unseen notification code processing....

--- client/src/Pages/Notification.jsx
+++ client/src/Pages/Notification.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import LeftSidebar from "../components/LeftSidebar";
 import RightSidebar from "../components/RightSidebar";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,10 +7,12 @@ import { getUserProfile } from "../api/api";
 import { setUser } from "../redux/userSlice";
 import Avatar from "react-avatar";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const Notification = () => {
   const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const [activeTab, setActiveTab] = useState("unSeen");
 
   const fetchUserProfileDetails = async () => {
     try {
@@ -19,17 +21,26 @@ const Notification = () => {
       if (res.success) {
         dispatch(hideLoading());
         dispatch(setUser(res.user));
-        console.log(res);
       }
     } catch (err) {
       dispatch(hideLoading());
-      console.log(err);
+      toast.error(err.response.data.message);
     }
   };
 
   useEffect(() => {
     fetchUserProfileDetails();
+    //eslint-disable-next-line
   }, []);
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const filteredNotifications =
+    activeTab === "unSeen"
+      ? user?.unSeenNotifications
+      : user?.seenNotifications;
 
   return (
     <>
@@ -37,28 +48,72 @@ const Notification = () => {
         <LeftSidebar />
         <div className="w-[50%] border-l border-r border-gray-200">
           <div className="p-4">
-            <h1 className="text-xl font-bold mb-4">Notifications</h1>
-            {/* Notification items */}
+            <h1 className="text-2xl font-bold mb-4">Notifications</h1>
+
+            {/***************  SEEN AND UNSEEN TAB  *********/}
+            <div className="flex space-x-4">
+              <button
+                onClick={() => handleTabChange("unSeen")}
+                className={`text-md font-semibold ${
+                  activeTab === "unSeen"
+                    ? "text-blue-500 border-b-4 border-blue-400"
+                    : "text-gray-500"
+                }`}
+              >
+                Unseen
+              </button>
+              <button
+                onClick={() => handleTabChange("seen")}
+                className={`text-md font-semibold ${
+                  activeTab === "seen"
+                    ? "text-blue-500 border-b-4 border-blue-400"
+                    : "text-gray-500"
+                }`}
+              >
+                Seen
+              </button>
+            </div>
+
+            {/****  MARK ALL AS SEEN AND DELETE ALL SEEN NOTIFICATIONS  *****/}
+            <div className="flex justify-end space-x-4 mb-3">
+              {activeTab === "unSeen" && (
+                <button className="text-sm text-blue-500 font-semibold">
+                  Mark all as seen
+                </button>
+              )}
+              {activeTab === "seen" && (
+                <button className="text-sm text-red-500 font-semibold">
+                  Delete all seen notifications
+                </button>
+              )}
+            </div>
+
             <div className="flex flex-col space-y-4">
-              {/* Notification item */}
-              {user?.unSeenNotifications.map((notifications) => (
-                <div className="flex items-center space-x-2">
-                  <Avatar
-                    src="https://static.dezeen.com/uploads/2023/07/x-logo-twitter-elon-musk_dezeen_2364_col_0.jpg"
-                    size="45"
-                    round={true}
-                  />
-                  <div>
-                    <Link
-                      to={`/profile/${notifications?.data?.loggedInUser?._id}`}
-                      className="font-semibold"
-                    >
-                      @{notifications?.data?.loggedInUser?.username}
-                    </Link>
-                    <p className="text-gray-600">{notifications?.message}!</p>
+              {/************ NOTIFICATIONS *****************/}
+              {filteredNotifications
+                ?.slice(0)
+                .reverse()
+                .map((notifications) => (
+                  <div
+                    key={notifications?.data?.loggedInUser._id}
+                    className="flex items-center space-x-2"
+                  >
+                    <Avatar
+                      src="https://static.dezeen.com/uploads/2023/07/x-logo-twitter-elon-musk_dezeen_2364_col_0.jpg"
+                      size="45"
+                      round={true}
+                    />
+                    <div>
+                      <Link
+                        to={`/profile/${notifications?.data?.loggedInUser?._id}`}
+                        className="font-semibold"
+                      >
+                        @{notifications?.data?.loggedInUser?.username}
+                      </Link>
+                      <p className="text-gray-600">{notifications?.message}!</p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         </div>
