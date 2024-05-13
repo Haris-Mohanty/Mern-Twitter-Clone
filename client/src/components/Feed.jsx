55d4a4d77cdd6a2b104f7515f406d50624c9d462
Haris Mohanty <harismohanty8658@gmail.com>
2024-05-13 20:29:03 +0530

    client- When no tweets found show no tweet found message.

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -9,9 +9,16 @@ const Feed = () => {
     <>
       <div className="w-[50%] border border-gray-200">
         <CreatePost />
-        {tweets?.map((tweet) => (
-          <Tweet key={tweet?._id} tweet={tweet} />
-        ))}
+        {tweets.length === 0 ? (
+          <div className="text-center py-8">
+            <h2 className="text-gray-500 text-lg font-semibold">
+              No tweets found.
+            </h2>
+            <p className="text-gray-400">Start sharing your thoughts now!</p>
+          </div>
+        ) : (
+          tweets?.map((tweet) => <Tweet key={tweet?._id} tweet={tweet} />)
+        )}
       </div>
     </>
   );
