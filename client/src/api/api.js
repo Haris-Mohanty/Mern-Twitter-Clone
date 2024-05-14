9df93596aa727d781d5671ef4be6cb81c1ac378e
Haris Mohanty <harismohanty8658@gmail.com>
2024-05-14 11:06:20 +0530

    client- api called successfully for get tweet details.

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -365,3 +365,20 @@ export const deleteAllSeenNotifications = async (id) => {
     throw err;
   }
 };
+
+//******* GET TWEET DETAILS ***********/
+export const getTweetDetails = async (id) => {
+  try {
+    const response = await axios.get(`/tweet/get-tweet-details/${id}`, {
+      withCredentials: true,
+    });
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error("Unexcepted Error Occurred!");
+    }
+  } catch (err) {
+    throw err;
+  }
+};
